720413ee14ab8379e569089339f2d53f6eaddb99
LaszloLajosT <track.runner800@outlook.com>
2020-05-02 12:00:15 +0100

    Add autoLink attribute to phone,email. Add alternative option using google maps(touching postcode)

--- app/src/main/java/com/example/athleticclub/MainActivity.java
+++ app/src/main/java/com/example/athleticclub/MainActivity.java
@@ -1,18 +1,29 @@
 package com.example.athleticclub;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void location(View view) {
+        Uri gmmIntentUri = Uri.parse("geo:0,0?q=BH7+6JD");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
     }
 }
